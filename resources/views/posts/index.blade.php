f2e36cd7d489b7401d0db0d257d0dd6e46474f4a
vldsy <vldsyata@gmail.com>
2024-10-11 15:32:06 +0200

    created likes feature

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,8 +29,21 @@
                 <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-6 text-sm">{{ $post->created_at->diffForHumans() }}</span>
 
                 <p class="mb-2">{{ $post->body }}</p>
+
+                <div class="flex items-center">
+                    <form action="" method="post" class="mr-1">
+                        @csrf
+                        <button type="submit" class="text-blue-500">Like</button>
+                    </form>
+                    <form action="" method="post" class="mr-1">
+                        @csrf
+                        <button type="submit" class="text-blue-500">Unlike</button>
+                    </form>
+                </div>
             </div>
         @endforeach
+
+        {{ $posts->links() }}
       @else
         <p>There are no posts</p>   
       @endif
