2b4c1914f798b475a948453c7efa3cc2213f3059
vldsy <vldsyata@gmail.com>
2024-10-11 13:46:10 +0200

    added remember option to auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -20,7 +20,7 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        if (!Auth::attempt($request->only('email', 'password'), $request->remember)) {
             return back()->with('status', 'Invalid login details');
         };
 
