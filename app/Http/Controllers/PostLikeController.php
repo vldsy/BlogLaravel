1787bf3a74bc3be5ba9e450901aa8e6dd3822923
vldsy <vldsyata@gmail.com>
2024-10-11 16:01:51 +0200

    likes

---
+++ app/Http/Controllers/PostLikeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostLikeController extends Controller
+{
+    public function store(Post $post)
+    {
+        dd();
+    }
+}
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,7 +31,7 @@
                 <p class="mb-2">{{ $post->body }}</p>
 
                 <div class="flex items-center">
-                    <form action="" method="post" class="mr-1">
+                    <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
                         @csrf
                         <button type="submit" class="text-blue-500">Like</button>
                     </form>
@@ -39,6 +39,8 @@
                         @csrf
                         <button type="submit" class="text-blue-500">Unlike</button>
                     </form>
+
+                    <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
                 </div>
             </div>
         @endforeach
